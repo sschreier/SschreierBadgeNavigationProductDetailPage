22ebfe235b2535324261f84246b29376c125b7c5
Sebastian Schreier <info@sebastianschreier.de>
2025-02-01 10:00:19 +0100

    php files updated

--- src/SschreierBadgeNavigationProductDetailPage.php
+++ src/SschreierBadgeNavigationProductDetailPage.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/*
+ * (c) Sebastian Schreier <info@sebastianschreier.de>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Sschreier\BadgeNavigationProductDetailPage;
 
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
--- src/Storefront/Subscriber/FrontendSubscriber.php
+++ src/Storefront/Subscriber/FrontendSubscriber.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/*
+ * (c) Sebastian Schreier <info@sebastianschreier.de>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Sschreier\BadgeNavigationProductDetailPage\Storefront\Subscriber;
 
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity;
